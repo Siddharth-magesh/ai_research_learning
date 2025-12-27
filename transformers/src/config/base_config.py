2ba06917de9fcd1fda2df24e5c3bdd25a502aceb
Siddharth-magesh <siddharthmagesh007@gmail.com>
2025-12-27 05:52:35 +0000

    feat: Add configuration files for model, training, and dataset, including BaseConfig class

---
+++ transformers/src/config/base_config.py
@@ -0,0 +1,14 @@
+import torch
+import os
+from dataclasses import dataclass
+
+@dataclass
+class BaseConfig:
+    project_name: str = "transformers-from-scratch"
+    experiment_name: str = "baseline"
+    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    seed: int = 42
+    num_workers: int = os.cpu.count()
+    log_interval: int = 100
+    save_interval: int = 1
+    
